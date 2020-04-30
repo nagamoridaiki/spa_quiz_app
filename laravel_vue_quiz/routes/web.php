3735417c7c3d37398008b879326c76c1bf8ac8a2
nagamoridaiki <no.swea1017@gmail.com>
2020-04-30 19:50:35 +0900

    会員登録機能の作成

--- laravel_vue_quiz/routes/web.php
+++ laravel_vue_quiz/routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
 Route::get('/quiz', function () {
     return view('quiz.index');
 });
+Route::get('/register', function () {
+    return view('auth.register.index');
+});
+Route::post('/register', 'Auth\RegisterController@register');
